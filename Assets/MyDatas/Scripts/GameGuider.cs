2b718661a2476a5e3a721f14f6b12e39b708d597
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-07-03 14:28:24 +0900

    Initial Project Commit

---
+++ Assets/MyDatas/Scripts/GameGuider.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameGuider : MonoBehaviour {
+
+    [SerializeField]
+    private float _typingSpeed = 0.1f;
+
+    [SerializeField]
+    private TextAsset[] _textFile;
+
+    private GameManager _gm;
+
+    private string _text;
+
+    private Text _textForUI;
+
+
+    // Use this for initialization
+    void Start () {
+        _gm = GameManager.instance;
+
+        _textForUI = transform.GetChild(0).GetChild(0).GetComponent<Text>();
+        _text = _textFile[_gm.Quest - 1].text;
+        StartCoroutine(AutoType());
+		
+	}
+    IEnumerator AutoType()
+    {
+        foreach(char letter in _text.ToCharArray())
+        {
+            _textForUI.text += letter;
+            yield return new WaitForSeconds(_typingSpeed);
+        }
+    }
+
+}
---
+++ Assets/MyDatas/Scripts/RobotHint.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RobotHint : MonoBehaviour {
+
+    public float interval = 5;
+    public float speed = 0.1f;
+
+    [SerializeField]
+    private Sprite[] _sprites;
+
+    private float _time;
+    private float _time2;
+    private void Start()
+    {
+        StartCoroutine(Move());
+
+    }
+
+    IEnumerator Move()
+    {
+        speed *= -1; 
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(Move());
+    }
+    // Update is called once per frame
+    void Update()
+    {
+         transform.position += new Vector3(0, speed, 0);
+
+        _time++;
+
+        if (_time >= 60 * Random.Range(3, interval))
+        {
+            _time2++;
+            gameObject.GetComponent<Image>().sprite = _sprites[1];
+
+            if (_time2 >= 3.0f)
+            {
+                _time = 0;
+                _time2 = 0;
+                gameObject.GetComponent<Image>().sprite = _sprites[0];
+            }
+        }
+    }
+}
